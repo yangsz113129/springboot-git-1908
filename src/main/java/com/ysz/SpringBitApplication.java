7b01aac3496f0cc9cd053e9d3691d18e865ae7b3
yangsz113129 <151986015@qq.com>
2020-05-24 22:23:49 +0800

    注é初始化

---
+++ src/main/java/com/ysz/SpringBitApplication.java
@@ -0,0 +1,13 @@
+package com.ysz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBitApplication.class, args);
+    }
+
+}
